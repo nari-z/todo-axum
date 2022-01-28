61c4359f6018f1b28a5b629a36b912399f483e41
nari-z <go.to18ishi@gmail.com>
2022-01-28 15:24:40 +0900

    impl sample API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    routing::get,
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use serde::Serialize;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health", get(health));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    hyper::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[derive(Debug, Serialize, Clone)]
+struct HealthResponse {
+    healthy: bool,
+}
+
+async fn health() -> impl IntoResponse {
+    (StatusCode::OK, Json(HealthResponse{healthy: true}))
 }
